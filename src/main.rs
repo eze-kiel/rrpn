6d9a8b31810f5031789bffa58e1237754a28af47
ezekiel <ezekiel@fastmail.com>
2021-05-27 11:47:15 +0200

    implemented multiple values in one line, resolves #3

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,30 @@ fn main() {
         print!("rrpn> ");
         io::stdout().flush().unwrap();
         let line: String = read!("{}\n");
-        match &line[..] {
-            "+" | "add" => ops::add(&mut stack),
-            "-" | "sub" => ops::sub(&mut stack),
-            "*" | "mul" => ops::mul(&mut stack),
-            "/" | "div" => ops::div(&mut stack),
-            "sum" => ops::sum(&mut stack),
-            "%" | "mod" => ops::modulo(&mut stack),
-            "mean" => ops::mean(&mut stack),
-            "swap" => ops::swap(&mut stack),
-            "c" | "clear" => ops::clear(&mut stack),
-            "d" | "drop" => ops::drop(&mut stack),
-            "q" | "quit" => ops::quit(),
-            "?" | "h" | "help" => ops::help(),
-            _ => {
-                let f = line.parse::<f64>();
-                let _f = match f {
-                    Ok(float) => stack.push(float),
-                    Err(_error) => println!("operation not recognised"),
-                };
+        let split = line.split(" ");
+        let vec: Vec<&str> = split.collect();
+
+        for item in vec.iter() {
+            match *item {
+                "+" | "add" => ops::add(&mut stack),
+                "-" | "sub" => ops::sub(&mut stack),
+                "*" | "mul" => ops::mul(&mut stack),
+                "/" | "div" => ops::div(&mut stack),
+                "sum" => ops::sum(&mut stack),
+                "%" | "mod" => ops::modulo(&mut stack),
+                "mean" => ops::mean(&mut stack),
+                "swap" => ops::swap(&mut stack),
+                "c" | "clear" => ops::clear(&mut stack),
+                "d" | "drop" => ops::drop(&mut stack),
+                "q" | "quit" => ops::quit(),
+                "?" | "h" | "help" => ops::help(),
+                _ => {
+                    let f = item.parse::<f64>();
+                    let _f = match f {
+                        Ok(float) => stack.push(float),
+                        Err(_error) => println!("operation not recognised"),
+                    };
+                }
             }
         }
         display_stack(&mut stack)
